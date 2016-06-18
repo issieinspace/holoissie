6123a678baba7e01c61742b65b47d5738b623296
mark laughery <mwlaugh@gmail.com>
2016-06-18 10:39:15 -0700

    Initial unity project and gitignore

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject Wormhole;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Check if no wormhole abort
+        if (Wormhole==null)
+        {
+            return;
+        }
+
+        // Get the position
+        float wormholeY = Wormhole.transform.position.y;
+        float cameraY = Camera.main.transform.position.y;
+
+        if(wormholeY - cameraY > .5)
+        {
+           // Get Wormhole behaviour script and call methods on it
+
+            
+
+        }
+    }
+
+    // ALIENS can look for gameplay manager
+    // 
+}
---
+++ Assets/Scripts/WormholeBehviour.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WormholeBehaviour : MonoBehaviour {
+
+    public GameObject AlienPrefab;
+
+	// Use this for initialization
+	void Start () {
+	    if(AlienPrefab != null)
+        {
+            GameObject alien = GameObject.Instantiate(AlienPrefab);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+}
