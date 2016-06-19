e351e8394c25f28576864e3f58b3436d27132a83
rashansmith <smith.rashan@gmail.com>
2016-06-18 21:37:01 -0700

    Alien movements

--- Assets/Scripts/AlienBehaviour.cs
+++ Assets/Scripts/AlienBehaviour.cs
@@ -11,24 +11,39 @@ public class AlienBehaviour : MonoBehaviour {
     public GameObject AlienPrefab;
     public Transform Target;
 
+    private Animation animator;
+
+    private AnimationClip currentAnimation;
 
     void Start ()
     {
-    
+        animator = this.GetComponent<Animation>();
+        currentAnimation = animator.GetClip("alien_idle");
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
 	    transform.LookAt(Target);
+        if(animator.isPlaying == false)
+        {
+            animator.Play(currentAnimation.name);
+        }
     }
 
     public void OnDrop()
     {
         var rigidbody = this.gameObject.AddComponent<Rigidbody>();
         rigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        currentAnimation = animator.GetClip("alien_walking");
     }
 
+
+    public void run()
+    {
+        currentAnimation = animator.GetClip("alien_running");
+        this.transform.position = Vector3.left;
+    }
   
 
 
