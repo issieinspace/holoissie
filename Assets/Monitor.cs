f6e85386aea5adb489370baa6f2663b386e3f859
Michael Doyle <m@mdoyle.com>
2016-08-29 09:02:19 -0400

    Adding a monitor object for diagnostic output

---
+++ Assets/Monitor.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Monitor : MonoBehaviour
+{
+
+    public bool Debug;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnGUI()
+    {
+
+        if (Debug)
+        {
+            // Make a background box
+            GUI.Box(new Rect(10, 10, 400, 400), "Actions");
+
+
+        }
+    }
+}
+
