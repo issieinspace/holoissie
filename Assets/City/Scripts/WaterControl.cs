0ebbcbb5de94d07c6e96217788ffe31108a0216e
leoricshen <leoricshen@gmail.com>
2017-04-29 16:43:20 -0400

    add water flow effect

---
+++ Assets/City/Scripts/WaterControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterControl : MonoBehaviour {
+    public GameObject water;
+	// Use this for initialization
+	void Start () {
+        water.transform.position = new Vector3(water.transform.position.x, 0, water.transform.position.z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        water.transform.position = new Vector3(water.transform.position.x, water.transform.position.y+0.001f, water.transform.position.z);
+    }
+}
