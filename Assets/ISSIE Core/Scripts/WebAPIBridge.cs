9373fd026192ae4a4f11f5a4e0ae8b94648793d8
Michael Doyle <m@mdoyle.com>
2018-04-14 15:14:31 -0700

    Introduce BestHTTP for WebAPIBridge

--- Assets/ISSIE Core/Scripts/WebAPIBridge.cs
+++ Assets/ISSIE Core/Scripts/WebAPIBridge.cs
@@ -1,40 +1,38 @@
-﻿using System.Collections;
-
-using HoloToolkit.Unity;
+﻿using BestHTTP;
+using System;
 using UnityEngine;
-using UnityEngine.Networking;
 
-public class WebAPIBridge : Singleton<WebAPIBridge> {
-
+public class WebAPIBridge : MonoBehaviour
+{
     public CycleReadout cycleReadout;
-    public string URL = "http://issiepi.local:9000/rpm_read";
+    public string TargetURL;
 
-    // Use this for initialization
-    void Start () {
-        StartCoroutine(GetCycleReadout());
-	}
-
-    private IEnumerator GetCycleReadout()
+    private void GetCycleReadout()
     {
-        string TargetURL = URL; //"http://issiepi.local:9000/rpm_read";
-        Debug.Log("Going to hit " + TargetURL);
-        UnityWebRequest webRequest = UnityWebRequest.Get(TargetURL);
-        yield return webRequest.SendWebRequest();
-        if (webRequest.isNetworkError)
-        {
-            Debug.Log(webRequest.error);
-        }
-        else
-        {
-            string data = System.Text.Encoding.UTF8.GetString(webRequest.downloadHandler.data);
-            Debug.Log("Got data: " + data);
-            GameObject.Find("Diagnostic").GetComponent<Monitor>().DisplayMessage(data);
-            cycleReadout = JsonUtility.FromJson<CycleReadout>(data);
-        }
+        
+        HTTPRequest request = new HTTPRequest(new Uri(TargetURL), OnRequestFinished);
+        request.Send();
+        
     }
 
-    // Update is called once per frame
-    void Update () {
-		
-	}
+    void OnRequestFinished(HTTPRequest request, HTTPResponse response)
+    {
+        string data = response.DataAsText;
+        cycleReadout = JsonUtility.FromJson<CycleReadout>(data);
+        WriteDiagnostics("Elapsed distance: " + cycleReadout.distance);
+        Debug.Log("Requested " + TargetURL + " Text received: " + response.DataAsText);
+    }
+
+    private void WriteDiagnostics(string data)
+    {
+       GameObject.Find("Diagnostic").GetComponent<Monitor>().DisplayMessage(data);
+    }
+
+    void Update()
+    {
+        if (Input.GetAxis("Fire1") > 0)
+        {
+            GetCycleReadout();
+        }
+    }
 }
