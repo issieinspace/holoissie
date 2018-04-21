40e53166946b4acb5461cd05f7b1c5bd70e2bde7
Michael Doyle <m@mdoyle.com>
2018-04-21 15:00:27 -0700

    Increased MVR intro volume and put a message for when CEVIS is offline

--- Assets/StartupDiagnostics.cs
+++ Assets/StartupDiagnostics.cs
@@ -7,7 +7,7 @@ public class StartupDiagnostics : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		
-	    HTTPRequest request = new HTTPRequest(new Uri("http://issiepi.local:9000"), OnRequestFinished);
+	    HTTPRequest request = new HTTPRequest(new Uri("http://192.168.86.78:9000"), OnRequestFinished);
         request.Send();
         
     }
