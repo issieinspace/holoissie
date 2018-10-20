4611bb19d57d1bec68ed804b7082d4c6c8f84b7e
goodkris <goodkris@yahoo.com>
2018-10-20 15:37:28 -0700

    Initial version

---
+++ Assets/Issie Tour De Earth/Scripts/BicycleManager.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum ViewingPane
+{
+    LeftPane,
+    RighPane,
+    FrontPane,
+    None
+}
+
+public class BicycleManager : MonoBehaviour {
+    public GameObject FrontPane;
+    public GameObject LeftPane;
+    public GameObject RightPane;
+    public float DistancePerPhoto = 800;
+    public float intervalMillis = 100;
+    public GameObject pictureBucket;
+    public PictureListManager pictureListManager;
+
+    private float elapsedMillis = 0;
+    private CycleReadout cycleReadOut;
+
+    private float leftPaneProgress = 0;
+    private float rightPaneProgress = 0;
+    private ViewingPane lastActivePane = ViewingPane.LeftPane;
+
+    private float distanceSinceLastUpdate;
+
+
+	// Use this for initialization
+	void Start () {
+        cycleReadOut = new CycleReadout();
+        pictureListManager = pictureBucket.GetComponent<PictureListManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // Gaze
+        RaycastHit hitInfo;
+        if (Physics.Raycast(
+                Camera.main.transform.position,
+                Camera.main.transform.forward,
+                out hitInfo,
+                20.0f,
+                Physics.DefaultRaycastLayers))
+        {
+            // If the Raycast has succeeded and hit a hologram
+            // hitInfo's point represents the position being gazed at
+            // hitInfo's collider GameObject represents the hologram being gazed at
+        }
+
+        if (hitInfo.collider.gameObject.tag == "LeftPictureFrame")
+        {
+            lastActivePane = ViewingPane.LeftPane;
+        }
+        else if(hitInfo.collider.gameObject.tag == "RightPictureFrame")
+        {
+            lastActivePane = ViewingPane.RighPane;
+        }
+
+        elapsedMillis += Time.deltaTime * 1000;
+        if(elapsedMillis > intervalMillis)
+        {
+            // update RPM/Distance/Speed
+            cycleReadOut.distance += 10;
+            cycleReadOut.rpm = 20;
+            cycleReadOut.speed = 14;
+            distanceSinceLastUpdate = 10;
+            elapsedMillis = 0;
+        }
+        UpdateLeftPane();
+        UpdateRightPane();
+        UpdateFrontPane();
+	}
+
+    void UpdateLeftPane()
+    {
+        if(lastActivePane == ViewingPane.LeftPane)
+        {
+            leftPaneProgress += distanceSinceLastUpdate;
+            LeftPane.GetComponentInChildren<Image>().fillAmount = leftPaneProgress/DistancePerPhoto;
+            if(leftPaneProgress > DistancePerPhoto)
+            {
+                leftPaneProgress = 0;
+                SetNextPicture(LeftPane.GetComponentInChildren<Image>(), pictureListManager.PictureSprites[(int)(Random.value * pictureListManager.PictureSprites.Count)]);
+            }
+        }
+    }
+
+    void SetNextPicture(Image image, Sprite sprite)
+    {
+        image.sprite = sprite;
+    }
+
+    void UpdateRightPane()
+    {
+        if(lastActivePane == ViewingPane.RighPane)
+        {
+            rightPaneProgress += distanceSinceLastUpdate;
+            RightPane.GetComponentInChildren<Image>().fillAmount = rightPaneProgress / DistancePerPhoto;
+        }
+    }
+
+    void UpdateFrontPane()
+    {
+
+    }
+}
---
+++ Assets/Issie Tour De Earth/Scripts/PictureListManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PictureListManager : MonoBehaviour {
+
+    public List<Sprite> PictureSprites;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
